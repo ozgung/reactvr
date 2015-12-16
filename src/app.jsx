37d7b16349d2a3eb8ac59e7a03956ea297cc4300
Ozgun Genc <ozgungenc@gmail.com>
2015-12-16 13:39:51 +0200

    shadow support

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,7 @@ var Scene = ReactTHREE.Scene;
 var PerspectiveCamera = ReactTHREE.PerspectiveCamera;
 var MeshFactory = React.createFactory(ReactTHREE.Mesh);
 var DirectionalLight = ReactTHREE.DirectionalLight;
+var SpotLight = ReactTHREE.SpotLight;
 var Object3D = ReactTHREE.Object3D;
 var Mesh = ReactTHREE.Mesh;
 
@@ -15,10 +16,11 @@ var typeface = require('three.regular.helvetiker');
 THREE.typeface_js.loadFace(typeface);
 
 var white = 0xffffff;
+var orange = 0xff5500;
 
 var boxgeometry = new THREE.BoxGeometry( 200,200,200);
 var spheregeometry = new THREE.SphereGeometry( 200,200,200);
-var cylindergeometry = new THREE.CylinderGeometry( 100, 100, 100, 100);
+var cylindergeometry = new THREE.CylinderGeometry( 102, 100, 40, 100);
 var planegeometry = new THREE.PlaneGeometry( 10000, 10000, 100, 100);
 var textgeometry = new THREE.TextGeometry("asdfasdfsf",{font: 'helvetiker'});
 
@@ -32,22 +34,22 @@ var creamtexture = THREE.ImageUtils.loadTexture( assetpath('simple.png'));
 var floormaterial = new THREE.MeshPhongMaterial( { map: floortexture } );
 var solidmaterial = new THREE.MeshBasicMaterial( { map: floortexture  } );
 var creammaterial = new THREE.MeshPhongMaterial({map: creamtexture});
+var orangematerial = new THREE.MeshPhongMaterial({color: orange});
 
 // Robo component
 var Robo = React.createClass({
   displayName: 'Robo',
   propTypes: {
     position: React.PropTypes.instanceOf(THREE.Vector3),
-    quaternion: React.PropTypes.instanceOf(THREE.Quaternion).isRequired
+    quaternion: React.PropTypes.instanceOf(THREE.Quaternion).isRequired,
+    quaternion2: React.PropTypes.instanceOf(THREE.Quaternion)
   },
   render: function() {
-    return React.createElement(
-      ReactTHREE.Object3D,
-      {position:this.props.position || new THREE.Vector3(0,0,0), castShadow: true, receiveShadow:true },
-      MeshFactory({position:new THREE.Vector3(0, +100,0), scale: new THREE.Vector3(.5,.5,.5), geometry: spheregeometry, material:creammaterial}),
-      MeshFactory({position:new THREE.Vector3(0, +50,0), geometry:cylindergeometry, material:creammaterial}),
-      MeshFactory({position:new THREE.Vector3(0, -100,0), scale: new THREE.Vector3(1.05,1.05,1.05), quaternion:this.props.quaternion, geometry:spheregeometry, material:creammaterial})
-    );
+    return <Object3D position={this.props.position || new THREE.Vector3(0,0,0)} castShadow='true' receiveShadow='false'>
+      <Mesh position={new THREE.Vector3(0, +100,0)} scale={new THREE.Vector3(.5,.5,.5)} geometry={spheregeometry} material={creammaterial} quaternion={this.props.quaternion || new THREE.Quaternion()} castShadow='true' receiveShadow='false'/>
+      <Mesh position={new THREE.Vector3(0, +95,0)} geometry={cylindergeometry} material={orangematerial} castShadow='true' receiveShadow='false' />
+      <Mesh position={new THREE.Vector3(0, -100,0)} scale={new THREE.Vector3(1.05,1.05,1.05)} quaternion={this.props.quaternion} geometry={spheregeometry} material={creammaterial} castShadow='true' receiveShadow='false' />
+    </Object3D>
   }
 });
 
@@ -56,13 +58,14 @@ var VRScene = React.createClass({
     render: function() {
       var aspectratio = this.props.width / this.props.height;
       var cameraprops = {fov:50, aspect:aspectratio, near:1, far:100000,
-        position:new THREE.Vector3(this.props.cupcakedata.position.x, this.props.cupcakedata.position.y+1000, this.props.cupcakedata.position.z+1000), lookat:this.props.cupcakedata.position};
-      return  <Scene width={this.props.width} height={this.props.height} camera="maincamera" shadowMapEnabled={true}>
+        position:new THREE.Vector3(this.props.cupcakedata.position.x, this.props.cupcakedata.position.y+800, this.props.cupcakedata.position.z+1000), lookat:this.props.cupcakedata.position};
+      return  <Scene width={this.props.width} height={this.props.height} camera="maincamera" shadowMapEnabled={false} >
                 <PerspectiveCamera name="maincamera" {...cameraprops} />
-                <Robo position={this.props.cupcakedata.position} quaternion={this.props.cupcakedata.quaternion} onKeyPress={this.keyHandler}  />
-                <DirectionalLight position={new THREE.Vector3(0,0,1)} color={white} intensity={1}  castShadow={true} />
-                <Object3D quaternion={new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 1, 0, 0 ), -Math.PI / 2.0 )} receiveShadow={true} >
-                  <Mesh position={new THREE.Vector3(0,0,0)} geometry={planegeometry} material={solidmaterial} />
+                <Robo position={this.props.cupcakedata.position} quaternion={this.props.cupcakedata.quaternion} onKeyPress={this.keyHandler} castShadow={true} receiveShadow={false} />
+                <DirectionalLight position={new THREE.Vector3(1000,1000,1000)} color={white} intensity={1} />
+                <SpotLight onlyShadow='true' position={new THREE.Vector3(-this.props.cupcakedata.position.x, this.props.cupcakedata.position.y+10000, -this.props.cupcakedata.position.z)} color={white} intensity={1}  castShadow='true' shadowCameraLeft={-1000} shadowCameraRight={1000} shadowCameraTop={10000} shadowCameraBottom={-10000} shadowCameraNear={1} shadowCameraFar={100000} shadowMapWidth={2048} shadowMapWidth={2048} />
+                <Object3D quaternion={new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 1, 0, 0 ), -Math.PI / 2 )} receiveShadow={false} >
+                  <Mesh position={new THREE.Vector3(0,0,0)} geometry={planegeometry} material={solidmaterial} receiveShadow={false} />
                 </Object3D>
               </Scene>;
     },
